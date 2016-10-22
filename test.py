e4e24f3603df927292b3f204ca29d1985d944bd1
dfm114 <dfm114@ic.ac.uk>
2016-10-22 20:53:30 +0100

    Add scripts to test
    Signed-off-by: dfm114 <dfm114@ic.ac.uk>

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-import pylab
-
-data = pylab.loadtxt("log.txt")
-pylab.plot(data)
-pylab.show()
+import matplotlib.pyplot as plt 
+import numpy as np
+plt.plot(*np.loadtxt("log.txt",unpack=True, usecols={0,4}), linewidth=2.0, label="act_1")
+#plt.plot(*np.loadtxt("log.txt",unpack=True, usecols={0,2}), linewidth=2.0, label="act_0")
+plt.plot(*np.loadtxt("log.txt",unpack=True, usecols={0,3}), linewidth=2.0, label="ref_1")
+#plt.plot(*np.loadtxt("log.txt",unpack=True, usecols={0,1}), linewidth=2.0, label="ref_0")
+plt.legend()
+plt.show()
---
+++ test.sh
@@ -0,0 +1,5 @@
+rm log.txt
+
+sftp -b getLogTxt pi@129.31.180.46
+
+python test.py
